ab64f6c00c8b969081444336be41ebca5787b24c
Muhammad-ArifinDev <ipin93936@gmail.com>
2025-11-08 21:16:36 +0700

    membuat icon atau logo di login

---
+++ lib/screen/login.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 50),
+
+             
+              Center(
+                child: Image.asset(
+                  "assets/images/padlock.png",
+                  width: 120,
+                  height: 120,
+                  fit: BoxFit.cover,
+                ),
+              ),
+
+              const SizedBox(height: 30),
+              
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
