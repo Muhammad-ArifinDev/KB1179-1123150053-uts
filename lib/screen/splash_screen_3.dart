4f55edd49fe85f71ce6558dc74306f9cbc4554c3
Muhammad-ArifinDev <ipin93936@gmail.com>
2025-11-08 22:49:35 +0700

    mengubah gambar splash 3 dan text

--- lib/screen/splash_screen_3.dart
+++ lib/screen/splash_screen_3.dart
@@ -1,10 +1,126 @@
 import 'package:flutter/material.dart';
 
+
 class Screen3 extends StatelessWidget {
   const Screen3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+        children: [
+          SizedBox(height: 50),
+          Container(
+            width: 240,
+            height: 240,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.amber,
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/image3.jpeg"),
+                    fit: BoxFit.cover,
+                  ),
+            ),
+          ),
+          const SizedBox(height: 20),
+
+              // 🔹 Judul
+              const Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              // 🔹 Subtitle 2 baris
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "lets try wallie now!\n And get the best solution",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                    height: 1.5,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              // 🔹 3 Bullet Indicator
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFF84a98c), // aktif
+                    ),
+                  ),
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFedede9), // non-aktif
+                    ),
+                  ),
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFedede9), // non-aktif
+                    ),
+                  ),
+                ],
+              ),
+
+              const Spacer(),
+
+              // 🔹 Tombol Continue
+              SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.only(left: 40, right: 40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 11, 231, 59),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {
+                      
+                    },
+                    child: const Text(
+                      "Continue",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 70),
+
+        ],
+      ),
+        ),
+      ),
+    );
   }
 }
